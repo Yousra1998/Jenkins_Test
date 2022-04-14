7ab31d70b5fe769540f3f39dcf74853225159fe1
Yousra1998 <71673431+Yousra1998@users.noreply.github.com>
2022-04-14 13:32:57 +0000

    commit changes

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-
+//this class is for running the application by using run function
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
